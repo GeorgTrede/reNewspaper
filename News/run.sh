f84b0a03fc8853e0cf50d291337f1801481c4599
GeorgTrede <38717655+GeorgTrede@users.noreply.github.com>
2021-08-23 14:34:29 +1000

    Add files via upload

--- News/run.sh
+++ News/run.sh
@@ -8,11 +8,11 @@ fi
 id_of_news_doc=09dc93df-ce0f-4e28-976f-934b2ed5acd5
 
 echo ""
-echo "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"
-echo "█ ▄▄▀█ ▄▄██ ▀██ █ ▄▄█ ███ █ ▄▄█▀▄▄▀█ ▄▄▀█▀▄▄▀█ ▄▄█ ▄▄▀"
-echo "█ ▀▀▄█ ▄▄██ █ █ █ ▄▄█▄▀ ▀▄█▄▄▀█ ▀▀ █ ▀▀ █ ▀▀ █ ▄▄█ ▀▀▄"
-echo "█▄█▄▄█▄▄▄██ ██▄ █▄▄▄██▄█▄██▄▄▄█ ████▄██▄█ ████▄▄▄█▄█▄▄"
-echo "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"
+echo "█ ▄▄▀█ ▄▄██ ▀██ █ ▄▄█ ███ █ ▄▄█▀▄▄▀█ ▄▄▀█▀▄▄▀█ ▄▄█ ▄▄▀█"
+echo "█ ▀▀▄█ ▄▄██ █ █ █ ▄▄█▄▀ ▀▄█▄▄▀█ ▀▀ █ ▀▀ █ ▀▀ █ ▄▄█ ▀▀▄█"
+echo "█▄█▄▄█▄▄▄██ ██▄ █▄▄▄██▄█▄██▄▄▄█ ████▄██▄█ ████▄▄▄█▄█▄▄█"
+echo "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
 echo ""
 
 
@@ -28,23 +28,34 @@ echo "Starting to generate newspaper..."
 echo "Fetching articles from website..."
 python3 news.py
 echo "TeX file generated, trying to generate pdf file now..."
-latexmk -f -pdf -silent News.tex &> log_file.txt
 
-if grep -i "error" log_file.txt &> /dev/null
-then
-	echo ""
-	echo "An ERROR occured while generating the pdf. It is stored in 'log_file.txt':"
-	cat log_file.txt
-else
-	mv News.pdf Newspaper.pdf
-	rm News.*
-	mv Newspaper.pdf News.pdf
-	echo "Done"
-	echo "Uploading to reMarkable now..."
 
-	scp News.pdf root@$ip:/home/root/.local/share/remarkable/xochitl/$id_of_news_doc.pdf
-	ssh root@$ip "rm /home/root/.local/share/remarkable/xochitl/$id_of_news_doc.thumbnails/*" &> /dev/null
+for i in {1..5}
+do
+	latexmk -f -pdf -silent News.tex &> log_file.txt
 
-	echo "Done"
-	echo "Enjoy your day!"
-fi
+	if grep -i "error" log_file.txt &> /dev/null
+	then
+		if [ $i -eq 5 ]
+		then
+			echo ""
+			echo "An ERROR occured while generating the pdf. It is stored in 'log_file.txt':"
+			cat log_file.txt
+		else
+			echo -en "Attempt $i of 5 failed, trying again...\r"
+		fi
+	else
+		mv News.pdf Newspaper.pdf
+		rm News.*
+		mv Newspaper.pdf News.pdf
+		echo "Done                                  "
+		echo "Uploading to reMarkable now..."
+
+		scp News.pdf root@$ip:/home/root/.local/share/remarkable/xochitl/$id_of_news_doc.pdf
+		ssh root@$ip "rm /home/root/.local/share/remarkable/xochitl/$id_of_news_doc.thumbnails/*" &> /dev/null
+	
+		echo "Done"
+		echo "Enjoy your day!"
+		exit
+	fi
+done

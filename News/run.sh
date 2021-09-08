aabc47a6a129717405de340fb366f2b4477893a6
GeorgTrede <38717655+GeorgTrede@users.noreply.github.com>
2021-09-08 15:52:23 +1000

    Add files via upload

--- News/run.sh
+++ News/run.sh
@@ -1,12 +1,3 @@
-if [ $# -eq 0 ]
-then
-	ip=10.11.99.1
-else
-	ip=$1
-fi
-
-id_of_news_doc=09dc93df-ce0f-4e28-976f-934b2ed5acd5
-
 echo ""
 echo "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"
 echo "█ ▄▄▀█ ▄▄██ ▀██ █ ▄▄█ ███ █ ▄▄█▀▄▄▀█ ▄▄▀█▀▄▄▀█ ▄▄█ ▄▄▀█"
@@ -15,14 +6,6 @@ echo "█▄█▄▄█▄▄▄██ ██▄ █▄▄▄██▄█▄█
 echo "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
 echo ""
 
-
-if ping -c2 -t50 $ip &> /dev/null
-then
-	echo "Reached reMarkable ip, ready to start"
-else
-	echo "reMarkable couldn't be reached, make sure it's turned on and you entered the correct IP address!"
-	exit
-fi
 
 echo "Starting to generate newspaper..."
 echo "Fetching articles from website..."
@@ -49,10 +32,10 @@ do
 		rm News.*
 		mv Newspaper.pdf News.pdf
 		echo "Done                                  "
-		echo "Uploading to reMarkable now..."
+		echo "Uploading to reMarkable cloud now..."
 
-		scp News.pdf root@$ip:/home/root/.local/share/remarkable/xochitl/$id_of_news_doc.pdf
-		ssh root@$ip "rm /home/root/.local/share/remarkable/xochitl/$id_of_news_doc.thumbnails/*" &> /dev/null
+		rmapi rm News
+		rmapi put News.pdf
 	
 		echo "Done"
 		echo "Enjoy your day!"
